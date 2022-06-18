2293163377955cd51c06a360aa64b8bf08f6f6bb
Akeeb Hammed <akeebham@gmail.com>
2022-06-18 11:53:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0705246698",
+          email: "hammedakeeb@gmail.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "08123061496",
+          email: "hammedakeeb@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
